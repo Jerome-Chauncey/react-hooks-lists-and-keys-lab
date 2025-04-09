56ca2e10beb23c416f39cdb90cf974d05af2b6eb
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-04-09 23:02:40 +0300

    Implements Transforming a list of values into a list of components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,21 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linksWithKeys = links.map((link) => {
+    return (
+      
+        <a key={link} href={`#${link}`}>
+          {link}
+        </a>
+      
+    );
+  });
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <div>
+      <nav>{linksWithKeys}</nav>
+    </div>
+  )
 }
 
 export default NavBar;
